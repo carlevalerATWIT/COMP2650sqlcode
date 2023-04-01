dd22f331d49a8d3dd08b5f9eded5ad882eef3ca7
Ryder Carlevale <carlevaler@wit.edu>
2023-03-31 21:26:31 -0400

    123

--- CREATE.sql
+++ CREATE.sql
@@ -1,5 +1,3 @@
-Use homeimprovement;
-
 CREATE TABLE Department
 (
   DeptName VARCHAR(255) NOT NULL,
@@ -15,81 +13,23 @@ CREATE TABLE Store
   PRIMARY KEY (StoreID)
 );
 
-CREATE TABLE Products
-(
-  ProductName VARCHAR(255) NOT NULL,
-  ProductID INT NOT NULL,
-  SetPrice INT NOT NULL,
-  SalePrice INT,
-  SerialNumber INT NOT NULL,
-  Amount INT NOT NULL,
-  PRIMARY KEY (ProductID)
-);
-
 CREATE TABLE Shelves
 (
-  ShelfSection VARCHAR(255) NOT NULL,
+  SelfSection VARCHAR(255) NOT NULL,
   ShelfID INT NOT NULL,
   DeptID INT NOT NULL,
   PRIMARY KEY (ShelfID),
   FOREIGN KEY (DeptID) REFERENCES Department(DeptID)
 );
 
-CREATE TABLE Sales
-(
-  DailySaleAmount INT NOT NULL,
-  SaleDate DATE NOT NULL,
-  DeptID INT NOT NULL,
-  FOREIGN KEY (DeptID) REFERENCES Department(DeptID)
-);
-
-CREATE TABLE CustomerDelivery
-(
-  DeliveryDate DATE NOT NULL,
-  CustomerID INT NOT NULL,
-  DeliveryID INT NOT NULL,
-  ProductID INT NOT NULL,
-  PRIMARY KEY (DeliveryID),
-  FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
-);
-
-CREATE TABLE CustomerPickup
-(
-  PickupDate DATE NOT NULL,
-  CustoemrID INT NOT NULL,
-  PickupID INT NOT NULL,
-  ProductID INT NOT NULL,
-  PRIMARY KEY (PickupID),
-  FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
-);
-
 CREATE TABLE Customer
 (
-  CustomerPhone# INT NOT NULL,
+  CustomerPhone INT NOT NULL,
   HomeAddress VARCHAR(255) NOT NULL,
   LastName VARCHAR(255) NOT NULL,
   FirstName VARCHAR(255) NOT NULL,
   CustomerID INT NOT NULL,
-  ProductID INT NOT NULL,
-  PRIMARY KEY (CustomerID),
-  FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
-);
-
-CREATE TABLE ProductDelivery
-(
-  DeliveryDate DATE NOT NULL,
-  TruckID INT NOT NULL,
-  ProductID INT NOT NULL,
-  FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
-);
-
-CREATE TABLE BELONG_ON
-(
-  ProductID INT NOT NULL,
-  ShelfID INT NOT NULL,
-  PRIMARY KEY (ProductID, ShelfID),
-  FOREIGN KEY (ProductID) REFERENCES Products(ProductID),
-  FOREIGN KEY (ShelfID) REFERENCES Shelves(ShelfID)
+  PRIMARY KEY (CustomerID)
 );
 
 CREATE TABLE Employee
@@ -97,12 +37,12 @@ CREATE TABLE Employee
   EmployeeID INT NOT NULL,
   FirstName VARCHAR(255) NOT NULL,
   LastName VARCHAR(255) NOT NULL,
+  DeptID INT NOT NULL,
   Birthday DATE NOT NULL,
   ESSN INT NOT NULL,
   HomeAddress VARCHAR(255) NOT NULL,
   Salary INT NOT NULL,
   StoreID INT NOT NULL,
-  DeptID INT NOT NULL,
   PRIMARY KEY (EmployeeID),
   FOREIGN KEY (StoreID) REFERENCES Store(StoreID),
   FOREIGN KEY (DeptID) REFERENCES Department(DeptID)
@@ -114,4 +54,69 @@ CREATE TABLE Managers
   ESSN INT NOT NULL,
   EmployeeID INT NOT NULL,
   FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID)
+);
+
+CREATE TABLE Products
+(
+  ProductName VARCHAR(255) NOT NULL,
+  ProductID INT NOT NULL,
+  SetPrice INT NOT NULL,
+  SalePrice INT,
+  SerialNumber INT NOT NULL,
+  Amount INT NOT NULL,
+  ShelfID INT NOT NULL,
+  CustomerID INT NOT NULL,
+  PRIMARY KEY (ProductID),
+  FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID)
+);
+
+CREATE TABLE Sale
+(
+  DeptID INT NOT NULL,
+  SaleDate DATE NOT NULL,
+  AmountSold INT NOT NULL,
+  ProductID INT NOT NULL,
+  CustomerID INT NOT NULL,
+  FOREIGN KEY (ProductID) REFERENCES Products(ProductID),
+  FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID)
+);
+
+CREATE TABLE CustomerDelivery
+(
+  DeliveryDate DATE NOT NULL,
+  CustomerID INT NOT NULL,
+  DeliveryID INT NOT NULL,
+  CustomerDeliveryAmount INT NOT NULL,
+  ProductID INT NOT NULL,
+  PRIMARY KEY (DeliveryID),
+  FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
+
+CREATE TABLE CustomerPickup
+(
+  PickupDate DATE NOT NULL,
+  CustomerID INT NOT NULL,
+  PickupID INT NOT NULL,
+  CustomerPickupAmount INT NOT NULL,
+  ProductID INT NOT NULL,
+  PRIMARY KEY (PickupID),
+  FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
+
+CREATE TABLE ProductDelivery
+(
+  DeliveryDate DATE NOT NULL,
+  TruckID INT NOT NULL,
+  DeliveryAmount INT NOT NULL,
+  ProductID INT NOT NULL,
+  FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
+
+CREATE TABLE BELONG_ON
+(
+  ProductID INT NOT NULL,
+  ShelfID INT NOT NULL,
+  PRIMARY KEY (ProductID, ShelfID),
+  FOREIGN KEY (ProductID) REFERENCES Products(ProductID),
+  FOREIGN KEY (ShelfID) REFERENCES Shelves(ShelfID)
 );
