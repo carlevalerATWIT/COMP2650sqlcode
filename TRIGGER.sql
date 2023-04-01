fbdc73fbe46ff4e1dabbba17ff4117964bc270f7
Ryder Carlevale <carlevaler@wit.edu>
2023-03-31 21:30:54 -0400

    123

--- TRIGGER.sql
+++ TRIGGER.sql
@@ -21,3 +21,50 @@ BEGIN
     WHERE productid = NEW.productid;
 END; //
 DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER UpdateInventoryAfterProductDelivery
+AFTER INSERT ON ProductDelivery
+FOR EACH ROW
+BEGIN
+  UPDATE Products
+  SET Amount = Amount + NEW.DeliveryAmount
+  WHERE ProductID = NEW.ProductID;
+END; //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER UpdateDailySalesAfterDelivery
+AFTER INSERT ON CustomerDelivery
+FOR EACH ROW
+BEGIN
+  INSERT INTO Sale (DeptID, SaleDate, AmountSold, ProductID, CustomerID)
+  VALUES (
+    (SELECT DeptID FROM BELONG_ON WHERE ProductID = NEW.ProductID),
+    NEW.DeliveryDate,
+    NEW.CustomerDeliveryAmount,
+    NEW.ProductID,
+    NEW.CustomerID
+  )
+  ON DUPLICATE KEY UPDATE
+    AmountSold = AmountSold + NEW.CustomerDeliveryAmount;
+END; //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER UpdateDailySalesAfterPickup
+AFTER INSERT ON CustomerPickup
+FOR EACH ROW
+BEGIN
+  INSERT INTO Sale (DeptID, SaleDate, AmountSold, ProductID, CustomerID)
+  VALUES (
+    (SELECT DeptID FROM BELONG_ON WHERE ProductID = NEW.ProductID),
+    NEW.PickupDate,
+    NEW.CustomerPickupAmount,
+    NEW.ProductID,
+    NEW.CustomerID
+  )
+  ON DUPLICATE KEY UPDATE
+    AmountSold = AmountSold + NEW.CustomerPickupAmount;
+END; //
+DELIMITER ;
